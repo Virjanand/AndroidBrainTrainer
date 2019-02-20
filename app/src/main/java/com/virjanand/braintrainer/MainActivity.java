66a23bcf8fcc46ece0e8ef184216368f42134f77
Virjanand <virjanand@virjanand.com>
2019-02-20 04:47:27 +0100

    Linked countdown timer to countdown text

--- app/src/main/java/com/virjanand/braintrainer/MainActivity.java
+++ app/src/main/java/com/virjanand/braintrainer/MainActivity.java
@@ -1,13 +1,32 @@
 package com.virjanand.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView countDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countDown = (TextView) findViewById(R.id.timerTextView);
+
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long milliSecondsUntilDone) {
+                updateCountDown(milliSecondsUntilDone);
+            }
+            public void onFinish() {
+                updateCountDown(0);
+            }
+        }.start();
+    }
+
+    private void updateCountDown(long milliSecondsUntilDone) {
+        countDown.setText(milliSecondsUntilDone / 1000 + "s");
     }
 }
