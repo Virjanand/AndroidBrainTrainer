a53fa7085148ee43825fe341f5b762bfe9a8971c
Virjanand <virjanand@virjanand.com>
2019-02-22 05:14:09 +0100

    Added button that starts countdownTimer

--- app/src/main/java/com/virjanand/braintrainer/MainActivity.java
+++ app/src/main/java/com/virjanand/braintrainer/MainActivity.java
@@ -3,11 +3,17 @@ package com.virjanand.braintrainer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView countDown;
+    private CountDownTimer countDownTimer;
+
+    public void startGame(View view) {
+        countDownTimer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         countDown = (TextView) findViewById(R.id.timerTextView);
 
-        new CountDownTimer(10000, 1000) {
+        countDownTimer = new CountDownTimer(10000, 1000) {
             public void onTick(long milliSecondsUntilDone) {
                 updateCountDown(milliSecondsUntilDone);
             }
+
             public void onFinish() {
                 updateCountDown(0);
             }
-        }.start();
+        };
     }
 
     private void updateCountDown(long milliSecondsUntilDone) {
