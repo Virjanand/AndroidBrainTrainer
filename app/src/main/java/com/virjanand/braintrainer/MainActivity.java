9a89bcf1719b156075c2c7aee48bd527ef9839a0
Virjanand <virjanand@virjanand.com>
2019-03-08 06:14:22 +0100

    Show grid on start

--- app/src/main/java/com/virjanand/braintrainer/MainActivity.java
+++ app/src/main/java/com/virjanand/braintrainer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -27,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         countDownTimer.start();
         startGameButton.setVisibility(View.INVISIBLE);
         generateSum();
+        showGrid();
+    }
+
+    private void showGrid() {
+        GridLayout grid = (GridLayout) findViewById(R.id.answerGridLayout);
+        grid.setVisibility(View.VISIBLE);
     }
 
     private void generateSum() {
